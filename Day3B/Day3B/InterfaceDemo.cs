44c57064fb2eddfbc14915cbe2e7fe4f6094db64
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-04-30 12:50:38 +0530

    Add: Interface class

---
+++ Day3B/Day3B/InterfaceDemo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3B
+{
+    public interface InterfaceDemo
+    {
+        void InterfaceMetthod();
+    }
+}
--- Day3B/Day3B/Program.cs
+++ Day3B/Day3B/Program.cs
@@ -1,17 +1,25 @@
 ﻿namespace Day3B
 {
-    public class Program
+    public class Program : InterfaceDemo
     {
+        public void InterfaceMetthod()
+        {
+            Console.WriteLine("This is a interface method");
+        }
         public static void Main(string[] args)
         {
-            ConstructorTypes obj = new ConstructorTypes();
-            ConstructorTypes obj_One = new ConstructorTypes("Bruce",33); 
-            obj_One.Output();
+            //ConstructorTypes obj = new ConstructorTypes();
+            //ConstructorTypes obj_One = new ConstructorTypes("Bruce",33); 
+            //obj_One.Output();
             //Copy Constructor
-            ConstructorTypes obj_Two = new ConstructorTypes("Ferrari", "485Hp");
-            ConstructorTypes construct = new ConstructorTypes(obj_Two);
-            Console.WriteLine("Copy Constructor : " + construct.car);
-            Console.WriteLine("Copy Constructor : " + construct.Hp);
+            //ConstructorTypes obj_Two = new ConstructorTypes("Ferrari", "485Hp");
+            //ConstructorTypes construct = new ConstructorTypes(obj_Two);
+            //Console.WriteLine("Copy Constructor : " + construct.car);
+            //Console.WriteLine("Copy Constructor : " + construct.Hp);
+
+            //Interface
+            Program demo = new Program();
+            demo.InterfaceMetthod();
         }
     }
 }
